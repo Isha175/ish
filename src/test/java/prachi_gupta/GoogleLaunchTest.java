e611679b20f6d2556966d4f158f747fc5e8aa173
Isha Pareta <ishapareta22@gmail.com>
2024-09-15 16:23:51 +0530

    add profile in pom

---
+++ src/test/java/prachi_gupta/GoogleLaunchTest.java
@@ -0,0 +1,21 @@
+package prachi_gupta;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleLaunchTest {
+	@Test
+    public void verify() {
+    	WebDriverManager.chromedriver().setup();
+    	WebDriver driver=new ChromeDriver();
+    	driver.manage().window().maximize();
+    	driver.get("https://www.google.com/");
+    	String actual=driver.getTitle();
+    	String expected="Google";
+    	Assert.assertEquals(actual,expected);
+    	driver.close();
+    }
+}
